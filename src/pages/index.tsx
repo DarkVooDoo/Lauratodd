c7e9548328c594431fe192dce2adb1eca283df4d
DarkVooDoo <moisestazaro@gmail.com>
2023-02-21 23:15:00 +0100

    Revert "Deploy Fix"
    This reverts commit 6c43421db2de307bb9e8211d431dd91a36a1bc0a.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello</h1>  
+        <h1>Hello's</h1>  
       </main>
     </>
   )
